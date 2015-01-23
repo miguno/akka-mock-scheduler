9295d21c932c7f19db3c2c2b82156dc3d2b0d6a9
Michael G. Noll <michael@michael-noll.com>
2015-01-23 10:28:26 +0100

    Move FakeCancellable into separate class and add a test spec

--- src/main/scala/com/miguno/akka/testing/MockScheduler.scala
+++ src/main/scala/com/miguno/akka/testing/MockScheduler.scala
@@ -74,25 +74,4 @@ class MockScheduler(time: VirtualTime) extends Scheduler {
 
   }
 
-  private case class FakeCancellable() extends Cancellable {
-
-    /**
-     * No-op cancel, i.e. this method will do nothing.
-     *
-     * We decided to use such a noop-always-returns-false strategy instead of throwing NotImplementedErrors to
-     * make the use of [[FakeCancellable]] more convenient in "typical" testing code.
-     *
-     * @return Always returns false.
-     */
-    override def cancel(): Boolean = false
-
-    /**
-     * Because `cancel()` is currently a no-op this method will always return false.
-     *
-     * @return Always returns false.
-     */
-    override def isCancelled: Boolean = false
-
-  }
-
 }
---
+++ src/test/scala/com/miguno/akka/testing/FakeCancellableSpec.scala
@@ -0,0 +1,42 @@
+package com.miguno.akka.testing
+
+import org.scalatest.{GivenWhenThen, Matchers, FunSpec}
+
+class FakeCancellableSpec extends FunSpec with Matchers with GivenWhenThen {
+
+  describe("FakeCancellable") {
+
+    it("should return false when cancelled") {
+      Given("an instance")
+      val cancellable = FakeCancellable()
+
+      When("I cancel it")
+      val result = cancellable.cancel()
+
+      Then("then it returns false")
+      result should be(false)
+    }
+
+    it("isCancelled should return false when cancel was not called yet") {
+      Given("an instance")
+      val cancellable = FakeCancellable()
+
+      When("I ask whether it has been successfully cancelled")
+      Then("then it returns false")
+      cancellable should not be 'cancelled
+    }
+
+    it("isCancelled should return false when cancel was called already") {
+      Given("an instance")
+      val cancellable = FakeCancellable()
+      And("the instance was cancelled")
+      cancellable.cancel()
+
+      When("I ask whether it has been successfully cancelled")
+      Then("then it returns false")
+      cancellable should not be 'cancelled
+    }
+
+  }
+
+}
