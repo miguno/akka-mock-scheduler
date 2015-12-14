7e41685ea80cd2f98860aa1c29bb1f41b433346f
Michael MacFadden <michael@macfadden.org>
2015-12-13 21:17:17 -0800

    Fixed a minor typo in the scaladoc.

--- src/main/scala/com/miguno/akka/testing/MockCancellable.scala
+++ src/main/scala/com/miguno/akka/testing/MockCancellable.scala
@@ -7,12 +7,12 @@ private[testing] case class MockCancellable(scheduler: MockScheduler, task: Task
   private[this] var canceled: Boolean = false
   
   /**
-   * Possibly cancels this Cancellable.  If the cancellable has not already
+   * Possibly cancels this Cancellable.  If the Cancellable has not already
    * been canceled, or terminated after a single execution, then the 
    * cancellable will be canceled.  If cancel has already been called or
    * the task has already terminated, then no action will be taken.
    *
-   * @return True if the Cancellable was canceld by THIS invocation of the
+   * @return True if the Cancellable was canceled by THIS invocation of the
    *         cancel method, false otherwise.
    */
   override def cancel(): Boolean = {
